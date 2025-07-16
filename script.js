dffb39c9cf9ce2d3cb047778f7466866579692e2
daramsreya83@gmail.com <your.email@example.com>
2025-07-16 12:46:49 +0530

    non-repeating

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+ let charCount={};
+ for(let char of str){
+    charConunt[char]=(charCount[char]||0)+1;
+ }
+ for(let char of str){
+    if(charCount[char]===1){
+        return char;
+    }
+ }
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
